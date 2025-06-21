e3df6b9eab9355bc5c7a3c742bbea13d3d599210
baont <thaibaoo2105@gmail.com>
2025-06-21 20:37:17 +0700

    finish UI
    all hard coded

--- src/main/java/com/poly/Controller/AdminController.java
+++ src/main/java/com/poly/Controller/AdminController.java
@@ -7,8 +7,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
 	@GetMapping
 	public String index() {
-		return ("admin/index");
+		return "admin/index";
+	}
+
+	@GetMapping("/account")
+	public String account() {
+		return "admin/account/index";
+	}
+
+	@GetMapping("/role")
+	public String role() {
+		return "admin/role/index";
+	}
+
+	@GetMapping("/product")
+	public String product() {
+		return "admin/product/index";
+	}
+
+	@GetMapping("/category")
+	public String category() {
+		return "admin/category/index";
+	}
+
+	@GetMapping("/order")
+	public String order() {
+		return "admin/order/index";
 	}
 }
